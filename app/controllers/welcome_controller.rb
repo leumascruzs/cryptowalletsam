0d7390de2f1795e8a53000080956a43fba6de687
Samuel Augusto <saugustocruz@gmail.com>
2022-06-21 20:35:14 -0300

    init

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def index
+    cookies[:curso] = "Curso de Ruby on Rails [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails [SESSION]"
+    @meu_nome = params[:nome]
+    @curso= params[:curso]
+  end
+end
